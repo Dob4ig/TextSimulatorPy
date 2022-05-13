7b97bc2407829163840dcc2868b0b616293721a6
Вадим Дмитриев <wadsd234@gmail.com>
2022-05-13 12:45:08 +0300

    Макет класса Game

---
+++ TextGame/Game.py
@@ -0,0 +1,31 @@
+
+class Game():
+    
+    def __init__(self):
+        pass
+
+    @property
+    def greeting(self):
+        pass
+    @property
+    def help_msg(self):
+        pass
+    @property
+    def win_msg(self):
+        pass
+    @property
+    def loose_msg(self):
+        pass
+    
+    @greeting.setter
+    def greeting (self,new_greeting):
+        pass
+    @help_msg.setter
+    def help_msg(sellf,new_help_msg):
+        pass
+    @win_msg.setter
+    def win_msg(self,new_win_msg):
+        pass
+    @loose_msg.setter
+    def loose_msg(self,new_loose_msg):
+        pass
