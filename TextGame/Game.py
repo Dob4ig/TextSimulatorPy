41de9eb5649c4224d6095b120657bea2f9e87814
Вадим Дмитриев <wadsd234@gmail.com>
2022-05-13 22:40:05 +0300

    Прописал свойства класса Game

--- TextGame/Game.py
+++ TextGame/Game.py
@@ -1,38 +1,49 @@
 
+
 class Game():
-    
+
     def __init__(self):
-        pass
+        self._greeting=None
+        self._help_msg=None
+        self._win_msg=None
+        self._loose_msg=None
+        self._data_path=None
+
 
     @property
     def greeting(self):
-        pass
+        return self._greeting
     @property
     def help_msg(self):
-        pass
+        return self._help_msg
     @property
     def win_msg(self):
-        pass
+        return self._win_msg
     @property
     def loose_msg(self):
-        pass
+        return self._loose_msg
     @property
     def data_path(self):
-        pass
+        return self._data_path
 
 
     @greeting.setter
     def greeting (self,new_greeting):
-        pass
+        self._greeting=new_greeting
     @help_msg.setter
-    def help_msg(sellf,new_help_msg):
-        pass
+    def help_msg(self,new_help_msg):
+        self._help_msg=new_help_msg
     @win_msg.setter
     def win_msg(self,new_win_msg):
-        pass
+        self._win_msg=new_win_msg
     @loose_msg.setter
     def loose_msg(self,new_loose_msg):
-        pass
+        self._loose_msg=new_loose_msg
     @data_path.setter
     def data_path(self,new_data_path):
-        pass
+        self._data_path=new_data_path
+
+
+
+
+
