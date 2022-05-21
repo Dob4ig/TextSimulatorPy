d8d47e7ec3dff7733106d1c2c00e3a96b49daad4
Вадим Дмитриев <wadsd234@gmail.com>
2022-05-21 17:33:09 +0300

    .gitignore

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ from settings import game, player
 #Точка входа игрока.
 print(game.greeting)
 while(True):
-    pass
+    print(game.menu)
+    #Выбор пункта меню:
+    temp = input()
+    if temp == 1:
+        pass
+    elif temp == 2:
+        pass
