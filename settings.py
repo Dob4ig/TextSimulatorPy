aea9050d723d759e1d725fc015f3723843839ead
Вадим Дмитриев <wadsd234@gmail.com>
2022-05-21 10:21:42 +0300

    settings.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+from TextGame.Game import Game
+from TextGame.Player import Player
+
+
+game=Game()
+game.greeting="Добро пожаловать в симулятор человека!\n$help для помощи!"
+game._win_msg="Вы выиграли!"
+game.loose_msg="Вы проиграли!"
+game.help_msg="Ваша цель продержаться 100 дней!"#Редактировать в процессе!
+
+
+player=Player()
+
+
